ee7c061c4d53dd11ea3eddf56c90964669dbc719
Boris Khalfin <boriskh83@gmail.com>
2026-08-17 11:51:37 +0300

    Fix formatting in contact information section

--- pages/5_✉️_Contact.py
+++ pages/5_✉️_Contact.py
@@ -13,12 +13,12 @@ with col_info:
     st.markdown("""
     **Principal Investigator:** Professor Debra Toiber  
     **Department:** Department of Life Sciences  
-    **Faculty:** Faculty of Natural Sciences
+    **Faculty:** Faculty of Natural Sciences  
     **Institution:** Ben-Gurion University of the Negev  
     **Location:** Life Sciences Building (40) 
-    **Debbie's office:** Room 205
-    **Laboratory** Rooms: 204, 206, 210, -147, -151.
-    **Phone number:** (+972) 8 646 1371
+    **Debbie's office:** Room 205  
+    **Laboratory** Rooms: 204, 206, 210, -147, -151  
+    **Phone number:** (+972) 8 646 1371  
     **Email:** `toiber@bgu.ac.il`
     """)
 
