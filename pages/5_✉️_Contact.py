279763f091e372fd281142d55a0becdf9a40596e
Boris Khalfin <boriskh83@gmail.com>
2026-08-17 11:50:24 +0300

    Update contact information for Prof. Debra Toiber

--- pages/5_✉️_Contact.py
+++ pages/5_✉️_Contact.py
@@ -11,10 +11,14 @@ col_info, col_map = st.columns([1, 1])
 
 with col_info:
     st.markdown("""
-    **Principal Investigator:** Dr. Debra Toiber  
+    **Principal Investigator:** Professor Debra Toiber  
     **Department:** Department of Life Sciences  
+    **Faculty:** Faculty of Natural Sciences
     **Institution:** Ben-Gurion University of the Negev  
-    **Location:** Be'er Sheva, Israel  
+    **Location:** Life Sciences Building (40) 
+    **Debbie's office:** Room 205
+    **Laboratory** Rooms: 204, 206, 210, -147, -151.
+    **Phone number:** (+972) 8 646 1371
     **Email:** `toiber@bgu.ac.il`
     """)
 
